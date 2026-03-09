b620ba28f3cff6e27132f1729f9b30fb75c98053
Fedor Shankov <fshankov@gmail.com>
2026-03-09 10:35:16 +0100

    Switch from Vercel to Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
-import vercel from "@astrojs/vercel";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
   output: "server",
-  // Default adapter for Node/self-hosted.
-  // Swap for @astrojs/vercel or @astrojs/netlify when deploying.
-  adapter: vercel(),
+  adapter: netlify(),
 });
